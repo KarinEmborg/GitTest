ad95821e07c2a3ed9b478461cddc51659d2c37f1
Karin Emborg <karinemborg@karins-macbook-pro.local>
2016-02-28 17:42:05 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Karin Emborg on 28/02/2016.
+//  Copyright © 2016 Karin Emborg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
